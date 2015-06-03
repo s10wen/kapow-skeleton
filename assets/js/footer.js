769b63724b3e775257a3122193c1eae3587cf344
Dave Green <hello@davetgreen.me>
2015-06-03 20:47:29 +0100

    Create footer.js

---
+++ assets/js/footer.js
@@ -0,0 +1,14 @@
+// Custom JS for the Footer
+// ----------------------------------------------------------------------------
+
+// Document Ready
+// -------------------------------------
+jQuery(function($) {
+
+}(jQuery));
+
+// Window Load
+// -------------------------------------
+jQuery(window).load(function($) {
+
+}(jQuery));
