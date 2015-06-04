85794f1dad046be641eab49af282027fcd086d4e
David Green <davetgreen@GALLIFREY.local>
2015-06-04 18:23:54 +0100

    More Yeoman updates.

--- generator-kapow/app/index.js
+++ generator-kapow/app/index.js
@@ -4,6 +4,7 @@
 
 'use strict';
 var chalk  = require('chalk');
+var wiring = require('html-wiring');
 var yeoman = require('yeoman-generator');
 var yosay  = require('yosay');
 
@@ -41,7 +42,7 @@ module.exports = yeoman.generators.Base.extend({
         type: 'input',
         name: 'projAuthor',
         message: chalk.yellow('(3/10)') + ' Who is the author of this project? ' + chalk.red('e.g. Bruce Wayne <bruce@gotham-ent.com>'),
-        default: 'my-project'
+        default: 'Author Name'
       },
       {
         type: 'list',
@@ -116,18 +117,18 @@ module.exports = yeoman.generators.Base.extend({
         type: 'input',
         name: 'projDBName',
         message: chalk.yellow('(8/10)') + ' What should I call the WordPress database? ' + chalk.red('e.g. gotham_ent'),
-        default: "my_project"
+        default: "my-project-db"
       },
       {
         type: 'input',
-        name: 'projDBEmail',
+        name: 'projAdminEmail',
         message: chalk.yellow('(9/10)') + ' What is the email address for the WordPress administrator?',
-        default: "hello@myproject.com"
+        default: "hello@my-project.com"
       },
       {
         type: 'input',
         name: 'projDomain',
-        message: chalk.yellow('(10/10)') + ' What name do you prefer for the Vagrant \'.dev\' domain? ' + chalk.red('e.g. gotham-ent'),
+        message: chalk.yellow('(10/10)') + ' What name (without any suffix) do you prefer for the Vagrant \'.dev\' domain? ' + chalk.red('e.g. gotham-ent'),
         default: "my-project"
       },
     ];
@@ -144,7 +145,7 @@ module.exports = yeoman.generators.Base.extend({
       this.projDocs       = answers.projDocs;
       this.projRespImages = answers.projRespImages;
       this.projDBName     = answers.projDBName;
-      this.projDBEmail    = answers.projDBEmail;
+      this.projAdminEmail = answers.projAdminEmail;
       this.projDomain     = answers.projDomain;
 
       // Congratulate the user
@@ -165,9 +166,10 @@ module.exports = yeoman.generators.Base.extend({
 
     var shortName = this.projShortName;
 
-    // Get Kapow! repositories
+    // Pull down Kapow! repositories
     // -------------------------------------
-    this.log(chalk.yellow('Pulling down a copy of Kapow! Grunt'));
+
+    this.log(chalk.yellow('Pulling down the Kapow! repositories'));
 
     // Grunt
     this.remote('mkdo', 'kapow-grunt', 'master', function(err, remote) {
@@ -183,6 +185,82 @@ module.exports = yeoman.generators.Base.extend({
     this.remote('mkdo', 'kapow-theme', 'master', function(err, remote) {
       remote.bulkDirectory('.', 'htdocs/wp-content/themes/' + shortName);
     });
+
+    // Update strings in project files
+    // -------------------------------------
+
+    this.log(chalk.yellow('Updating project files'));
+
+    var properRegex = /(My Project)/mg;
+    var shortRegex  = /(my-project)/mg;
+    var domainRegex = /(my-project.dev)/mg;
+    var dbnameRegex = /(my-project-db)/mg;
+    var emailRegex  = /(hello@my-project.com)/mg;
+
+    // .gitignore
+    var gitignorePath = ".gitignore",
+        gitignoreFile = wiring.readFileAsString(gitignorePath);
+
+    gitignoreFile = gitignoreFile.replace(properRegex, this.projProperName);
+    gitignoreFile = gitignoreFile.replace(shortRegex, this.projShortName);
+
+    wiring.writeFileFromString(gitignoreFile, gitignorePath);
+
+    // bower.json
+    var bowerPath = "bower.json",
+        bowerFile = wiring.readFileAsString(bowerPath);
+
+    bowerFile = bowerFile.replace(shortRegex, this.projShortName);
+    bowerFile = bowerFile.replace('Author Name', this.projAuthor);
+
+    wiring.writeFileFromString(bowerFile, bowerPath);
+
+    // gruntfile.js
+    var gruntfilePath = "gruntfile.js",
+        gruntfileFile = wiring.readFileAsString(gruntfilePath);
+
+    gruntfileFile = gruntfileFile.replace(properRegex, this.projProperName);
+    gruntfileFile = gruntfileFile.replace(shortRegex, this.projShortName);
+
+    wiring.writeFileFromString(gruntfileFile, gruntfilePath);
+
+    // package.json
+    var packagePath = "package.json",
+        packageFile = wiring.readFileAsString(packagePath);
+
+    packageFile = packageFile.replace(shortRegex, this.projShortName);
+
+    wiring.writeFileFromString(packageFile, packagePath);
+
+    // vvv-hosts
+    var vvvhostsPath = "vvv-hosts",
+        vvvhostsFile = wiring.readFileAsString(vvvhostsPath);
+
+    vvvhostsFile = vvvhostsFile.replace(domainRegex, this.projDomain + ".dev");
+
+    wiring.writeFileFromString(vvvhostsFile, vvvhostsPath);
+
+    // vvv-init.sh
+    var vvvinitPath = "vvv-init.sh",
+        vvvinitFile = wiring.readFileAsString(vvvinitPath);
+
+    vvvinitFile = vvvinitFile.replace(properRegex, this.projProperName);
+    vvvinitFile = vvvinitFile.replace(domainRegex, this.projDomain + ".dev");
+    vvvinitFile = vvvinitFile.replace(dbnameRegex, this.projDBName);
+    vvvinitFile = vvvinitFile.replace(emailRegex, this.projAdminEmail);
+    vvvinitFile = vvvinitFile.replace(shortRegex, this.projShortName);
+
+    wiring.writeFileFromString(vvvinitFile, vvvinitPath);
+
+    // vvv-nginx.conf
+    var vvvnginxPath = "vvv-nginx.conf",
+        vvvnginxFile = wiring.readFileAsString(vvvnginxPath);
+
+    vvvnginxFile = vvvnginxFile.replace(domainRegex, this.projDomain + ".dev");
+
+    wiring.writeFileFromString(vvvnginxFile, vvvnginxPath);
+
+
   },
 
   // Install required dependencies
