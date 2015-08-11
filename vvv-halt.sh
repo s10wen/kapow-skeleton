d59726f9c0e29903b96db61544ba664761f07d7c
Dave Green <hello@davetgreen.me>
2015-08-11 12:18:08 +0100

    Added script to copy DB dump into the repo on vagrant halt

---
+++ vvv-halt.sh
@@ -0,0 +1,29 @@
+# Halt script for My Project
+# -----------------------------------------------------------------------------
+
+# Perform backup
+# -------------------------------------
+
+dbname="my_project_db"
+dbfile="$dbname.sql"
+backupsrc="database/backups"
+backuptgt="$(dirname $0)/database"
+user=$USER
+
+cd $backupsrc
+
+if [ -f $dbfile ]
+  then
+    cp $dbfile $backuptgt
+
+    cd $backuptgt
+
+    mv $dbfile "$dbname-$user.sql"
+
+    echo "My Project database file backed up to the repo!";
+
+  else
+    echo "No My Project database file available for backup!"
+fi
+
+
