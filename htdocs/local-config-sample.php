f33e5ae4baf5637764b3a46316c40d11be08ad60
Dave Green <hello@davetgreen.me>
2015-08-18 12:21:51 +0100

    Create local-config-sample.php

---
+++ htdocs/local-config-sample.php
@@ -0,0 +1,6 @@
+define( 'DB_NAME', 'my_project_db' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASSWORD', 'root' );
+define( 'DB_HOST', '127.0.0.1' );
+define( 'DB_CHARSET', 'utf8' );
+define('WP_DEBUG', 'true');
