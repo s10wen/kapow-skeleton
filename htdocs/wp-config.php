a48fe7682ec221abf91dc3dd239fe23c373c68f9
Dave Green <hello@davetgreen.me>
2015-08-27 12:52:05 +0100

    Code refactor to WP standards

--- htdocs/wp-config.php
+++ htdocs/wp-config.php
@@ -90,8 +90,8 @@ define( 'DISALLOW_FILE_EDIT', true );
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
-if ( ! defined( 'ABSPATH' ) )
-	define( 'ABSPATH', dirname(__FILE__) . '/' );
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' ); }
 
 /** Sets up WordPress vars and included files. */
 require_once( ABSPATH . 'wp-settings.php' );
