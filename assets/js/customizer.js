9d3b2fcd4de55f4ab993c5f2dad577cf8d20320e
Dave Green <hello@davetgreen.me>
2015-08-27 12:33:01 +0100

    Added customizer.js

---
+++ assets/js/customizer.js
@@ -0,0 +1,14 @@
+// Customizer JS for live-previews
+// ----------------------------------------------------------------------------
+
+jQuery( function( $ ) {
+
+  // // Enable live preview for an element
+  // // -------------------------------------
+  //  wp.customize('your_setting_id', function(value) {
+  //    value.bind(function(newval) {
+  //      $('.selector').html(newval);
+  //    });
+  //  });
+
+}( jQuery ) );
