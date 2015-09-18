909cd1204572d5d1064822fe5b2266a8ac5b76f3
Dave Green <hello@davetgreen.me>
2015-09-18 11:35:13 +0100

    Added plugin autoloader

---
+++ kapow-skeleton/build/wp-content/mu-plugins/loader.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Load must use plugins.
+ *
+ * @link https://codex.wordpress.org/Must_Use_Plugins
+ *
+ * @package My Project
+ */
+ 
+// This is an example of how to load a must use plugin. Add a require_once statement for
+// each must use plugin you have added.
+// require_once( plugin_dir_path( __FILE__ ) . 'plugin-folder-name/main-plugin-file.php' );
