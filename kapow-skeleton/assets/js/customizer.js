a44c9c91ae51e939136ea02f1e840864bedcb76a
Dave Green <hello@davetgreen.me>
2016-03-25 15:56:12 +0000

    Added live preview JS handler for logo in customizer

--- kapow-skeleton/assets/js/customizer.js
+++ kapow-skeleton/assets/js/customizer.js
@@ -1,14 +1,35 @@
 // Customizer JS for live-previews
 // ----------------------------------------------------------------------------
 
-jQuery( function( $ ) {
+jQuery(function($) {
 
-  // // Enable live preview for an element
-  // // -------------------------------------
-  //  wp.customize('your_setting_id', function(value) {
-  //    value.bind(function(newval) {
-  //      $('.selector').html(newval);
-  //    });
-  //  });
+    // // Enable live preview for an element
+    // // -------------------------------------
+    //  wp.customize('your_setting_id', function(value) {
+    //    value.bind(function(newval) {
+    //      $('.selector').html(newval);
+    //    });
+    //  });
 
-}( jQuery ) );
+    // Live preview for standard size logo.
+	// -------------------------------------
+    wp.customize('my_project_logo', function(value) {
+        value.bind(function(newval) {
+            var container = $('.site-logo');
+            var img = $('.site-logo img');
+
+            if (newval.length === 0) {
+                // Remove.
+                img.remove();
+            } else if (img.length === 0 && newval.length > 0) {
+                // Add.
+                newimg = $('<img/>');
+                newimg.attr('src', newval);
+                container.prepend(newimg);
+            } else {
+                // Change.
+                img.attr('src', newval);
+            }
+        });
+    });
+}(jQuery));
