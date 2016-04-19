15cd7e85d7bbc0583e9ffccec87133c68d292b0a
Dave Green <hello@davetgreen.me>
2016-04-19 14:02:07 +0100

    Auto init JS for Foundation if included for dev.

--- kapow-skeleton/assets/js/footer.js
+++ kapow-skeleton/assets/js/footer.js
@@ -3,41 +3,53 @@
 
 // Document Ready
 // -------------------------------------
-jQuery( function( $ ) {
+jQuery(function($) {
 
-  // Skip Link Focus Fix - Helps with accessibility for keyboard only users.
-  // Learn more: https://github.com/Automattic/My Project/pull/136
+    // Skip Link Focus Fix - Helps with accessibility for keyboard only users.
+    // Learn more: https://github.com/Automattic/My Project/pull/136
 
-  var is_webkit = navigator.userAgent.toLowerCase().indexOf( 'webkit' ) > -1,
-    is_opera = navigator.userAgent.toLowerCase().indexOf( 'opera' ) > -1,
-    is_ie = navigator.userAgent.toLowerCase().indexOf( 'msie' ) > -1;
+    var is_webkit = navigator.userAgent.toLowerCase().indexOf('webkit') > -1,
+        is_opera = navigator.userAgent.toLowerCase().indexOf('opera') > -1,
+        is_ie = navigator.userAgent.toLowerCase().indexOf('msie') > -1;
 
-  if ( ( is_webkit || is_opera || is_ie ) && document.getElementById && window.addEventListener ) {
-    window.addEventListener( 'hashchange', function() {
-      var id = location.hash.substring( 1 ),
-        element;
+    if ((is_webkit || is_opera || is_ie) && document.getElementById && window.addEventListener) {
+        window.addEventListener('hashchange', function() {
+            var id = location.hash.substring(1),
+                element;
 
-      if ( !( /^[A-z0-9_-]+$/.test( id ) ) ) {
-        return;
-      }
+            if (!(/^[A-z0-9_-]+$/.test(id))) {
+                return;
+            }
 
-      element = document.getElementById( id );
+            element = document.getElementById(id);
 
-      if ( element ) {
-        if ( !( /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) ) ) {
-          element.tabIndex = -1;
-        }
+            if (element) {
+                if (!(/^(?:a|select|input|button|textarea)$/i.test(element.tagName))) {
+                    element.tabIndex = -1;
+                }
 
-        element.focus();
-      }
-    }, false );
+                element.focus();
+            }
+        }, false);
 
-  }
+    }
 
-}( jQuery ) );
+    // Init JS for Foundation if being used.
+    // -------------------------------------
+    if (window.Foundation !== undefined ) {
+        $(document).foundation();
+    }
+
+}(jQuery));
+
+// Window Resize
+// -------------------------------------
+jQuery(window).resize(function($) {
+
+});
 
 // Window Load
 // -------------------------------------
-jQuery( window ).load( function( $ ) {
+jQuery(window).load(function($) {
 
-}( jQuery ) );
+}(jQuery));
