8f83eedb4e75ffcd7e87842c361ef1034c1181ab
Dave Green <hello@davetgreen.me>
2016-05-17 14:40:35 +0100

    Slight refactor of assets/js structure

--- kapow-skeleton/assets/js/parts/_navigation.js
+++ kapow-skeleton/assets/js/parts/_navigation.js
@@ -1,5 +1,5 @@
-// Navigation JS
-//
+// Navigation JS.
+// ----------------------------------------------------------------------------
 // Handles toggling the navigation menu for small screens and enables tab
 // support for dropdown menus.
 // ----------------------------------------------------------------------------
