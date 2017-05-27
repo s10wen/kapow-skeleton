1bb595144a3d48271a995edf3f329a8342490a33
Dave Green <hello@davetgreen.me>
2017-05-27 15:59:29 +0100

    Customizer and skip-link JS updates to absorb changes from _S theme.

--- kapow-skeleton/assets/js/customizer.js
+++ kapow-skeleton/assets/js/customizer.js
@@ -7,30 +7,44 @@ jQuery(function($) {
     // // for a particular selector or group of
     // // selectors.
     // // -------------------------------------
-    //  wp.customize('your_setting_id', function(value) {
-    //    value.bind(function(newval) {
-    //      $('.selector').html(newval);
+    //  wp.customize( 'your_setting_id', function( value ) {
+    //    value.bind( function( newval ) {
+    //      $( '.selector' ).text( newval );
     //    });
     //  });
 
-    // Live preview for standard size logo.
-	// -------------------------------------
-    wp.customize('my_project_logo', function(value) {
-        value.bind(function(newval) {
-            var container = $('.site-logo');
-            var img = $('.site-logo img');
+    // Site Title and Description.
+    // -------------------------------------
+    wp.customize( 'blogname', function( value ) {
+        value.bind( function( to ) {
+            $( '.site-title a' ).text( to );
+        } );
+    } );
 
-            if (newval.length === 0) {
+    wp.customize( 'blogdescription', function( value ) {
+        value.bind( function( to ) {
+            $( '.site-description' ).text( to );
+        } );
+    } );
+
+    // Site Logo.
+	// -------------------------------------
+    wp.customize( 'my_project_logo', function( value ) {
+        value.bind( function( newval ) {
+            var container = $( '.site-logo' );
+            var img = $( '.site-logo img' );
+
+            if ( newval.length === 0 ) {
                 // Remove.
                 img.remove();
-            } else if (img.length === 0 && newval.length > 0) {
+            } else if ( img.length === 0 && newval.length > 0 ) {
                 // Add.
-                newimg = $('<img/>');
-                newimg.attr('src', newval);
-                container.prepend(newimg);
+                newimg = $( '<img/>' );
+                newimg.attr( 'src', newval );
+                container.prepend( newimg );
             } else {
                 // Change.
-                img.attr('src', newval);
+                img.attr( 'src', newval );
             }
         });
     });
