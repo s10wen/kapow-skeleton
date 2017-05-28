d4520b5b10f0f113755beba9317491629218684d
Dave Green <hello@davetgreen.me>
2017-05-28 11:18:31 +0100

    Added WP Enforcer and associated config to introduce Git pre-commit hook checking.

--- kapow-skeleton/vvv-init.sh
+++ kapow-skeleton/vvv-init.sh
@@ -17,7 +17,7 @@ mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON my_project.* TO wp@loc
 # -------------------------------------
 if [ ! -d build/wp-admin ] || [ ! -d build/wordpress/wp-admin ]
 then
-	echo "Installing WordPress using WP CLI"
+	echo "Installing and configuring WordPress using WP CLI"
 	# Download the latest stable release of WordPress
 	wp core download --path=build/wordpress --allow-root
 	# Install the database tables and configure WordPress.
@@ -37,6 +37,16 @@ then
 	curl -O https://raw.githubusercontent.com/poststatus/wptest/master/wptest.xml
 	wp import wptest.xml --authors=skip
 	rm wptest.xml
+fi
+
+# Trigger the WP Enforcer script to
+# copy git hooks across if it has not
+# already been installed.
+# -------------------------------------
+if [ ! -d vendor/stevegrunwell/wp-enforcer ]
+then
+	echo "Copying Git hooks from WP Enforcer"
+	./vendor/bin/wp-enforcer
 fi
 
 # The Vagrant site setup script will
